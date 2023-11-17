bf351f0d8a68f99f8d66a2e64b8157ecdf6b5cef
Yang Heechan <heechy06@naver.com>
2023-11-17 07:38:46 +0000

    Change profits->values

--- src/crack.py
+++ src/crack.py
@@ -8,39 +8,39 @@ WEIGHT_RANGE = 20
 class Solution:
     # N: int -> number of item choices
     # weights: list -> list of each item's weight
-    # profits: list -> list of each item's profit
+    # values: list -> list of each item's profit
     # len(weights) and len(pofits) MUST EQuAL to N
-    def __init__(self, N, weights, profits, fitness=0):
+    def __init__(self, N, weights, value, fitness=0):
         self.N = N
         self.weights = weights
-        self.profits = profits
+        self.values = value
         self.fitness = fitness
     
     def evaluate(self):
-        self.fitness = fitness(self.N, self.items)
+        self.fitness = fitness(self)
     
     def __str__(self):
         out_str = "====== SOLUTION ======\n"
         out_str += "N: {}\n".format(self.N)
         out_str += "weights ({}): {}\n".format(len(self.weights), self.weights)
-        out_str += "profits ({}): {}\n".format(len(self.profits), self.profits)
+        out_str += "values ({}): {}\n".format(len(self.values), self.values)
         out_str += "fitness: {}\n".format(self.fitness)
         out_str += "======================"
 
         return out_str
 
 # generates one random solution
-# output: Solution (N, weights, profits)
+# output: Solution (N, weights, values)
 def random_solution():
     N = random.randint(1, N_RANGE)
     weights = [random.randint(1, WEIGHT_RANGE) for i in range(N)]
 
-    profits = []
+    values = []
     x = [random.random() for i in range(N)]
     for i in range(N):
-        profits.append(int(weights[i]*(x[i]*3+0.5)))
+        values.append(int(weights[i]*(x[i]*3+0.5)))
     
-    sol = Solution(N, weights, profits)
+    sol = Solution(N, weights, values)
     
     return sol
     
@@ -62,8 +62,6 @@ import {sample}
         res_list.append(eval(code))
     return gt_sol, res_list
 
-
-
 # input: solution
 # output: fitness value
 def fitness(solution: Solution):
@@ -84,17 +82,17 @@ def select(k, population):
 
 def unzip(d):
     weights = [i for i, j in d]
-    profits = [j for i, j in d]
+    values = [j for i, j in d]
 
-    return weights, profits
+    return weights, values
 
 def crossover(p1: Solution, p2: Solution, rate=0.7):
     # TODO: Heechan
 
     if random.random() < rate:
-        # zip (weights, profits) as a pair
-        t1 = list(zip(p1.weights, p1.profits))
-        t2 = list(zip(p2.weights, p2.profits))
+        # zip (weights, values) as a pair
+        t1 = list(zip(p1.weights, p1.values))
+        t2 = list(zip(p2.weights, p2.values))
 
         # randomly select a cross over point to split
         cp1 = random.randint(0, p1.N-1)
@@ -105,8 +103,8 @@ def crossover(p1: Solution, p2: Solution, rate=0.7):
         c2 = t2[:cp2] + t1[cp1:]
 
         # unzip cross overed pair
-        c1_weights, c1_profits = unzip(c1)
-        c2_weights, c2_profits = unzip(c2)
+        c1_weights, c1_values = unzip(c1)
+        c2_weights, c2_values = unzip(c2)
 
         # make an offspring
         o1 = cp_sol(p1)
@@ -114,9 +112,9 @@ def crossover(p1: Solution, p2: Solution, rate=0.7):
 
         # assign cross over weight and profit to offspring
         o1.weights = c1_weights
-        o1.profits = c1_profits
+        o1.values = c1_values
         o2.weights = c2_weights
-        o2.profits = c2_profits
+        o2.values = c2_values
 
         # update N for the offspring to new cross over weight and profit
         o1.N = len(o1.weights)
@@ -131,17 +129,17 @@ def crossover(p1: Solution, p2: Solution, rate=0.7):
     return p1, p2
 
 def cp_sol(s):
-    return Solution(s.N, s.weights.copy(), s.profits.copy(), s.fitness)
+    return Solution(s.N, s.weights.copy(), s.values.copy(), s.fitness)
 
 def mutate(s: Solution, rate=0.7):
     m = cp_sol(s)
 
-    # Iterate over each element, weights, profits
+    # Iterate over each element, weights, values
     for i in range(m.N):
         if random.random() < rate:
             # Apply a small random change to the gene
             m.weights[i] += random.choice([-1, 1])
-            m.profits[i] += random.choice([-1, 1])
+            m.values[i] += random.choice([-1, 1])
     
     m.evaluate
 
