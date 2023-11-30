07344d23644a548b01ef29f66876fee214edef5e
Yang Heechan <heechy06@naver.com>
2023-11-30 10:38:57 +0000

    Remove program14 and 16

--- src/crack.py
+++ src/crack.py
@@ -1,10 +1,10 @@
 from utils import ga
 import sys
 
-N_MAX = 100
-B_MAX = 100000
-WEIGHT_MAX = 100000
-VALUE_MAX = 10000
+N_MAX = 50
+B_MAX = 50000
+WEIGHT_MAX = 50000
+VALUE_MAX = 5000
 
 # **************************
 # ********** MAIN **********
@@ -12,9 +12,9 @@ VALUE_MAX = 10000
 
 if __name__ == "__main__":
     best_sol = ga.ga(
-        experiment_name='hyunsun_01',
-        end_criterion=0.95,
-        tc_size=10, # greater than 2
+        experiment_name='norm_cp_02',
+        end_criterion=1.0,
+        tc_size=5, # greater than 2
         gen_limit=sys.maxsize,
         selection=2, # less than tc_size
         cross_rate=1.0,
@@ -26,6 +26,6 @@ if __name__ == "__main__":
         b_max=B_MAX,
         weight_max=WEIGHT_MAX,
         value_max=VALUE_MAX,
-        method = 4 # 1(random), 2(budget proportion), 3(norm CP select), 4(apply both)
+        method = 3 # 1(random), 2(budget proportion), 3(norm CP select), 4(apply both)
     )
 
